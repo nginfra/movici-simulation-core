44d1179c584bd1112118712e61efded285b69115
Pelle Koster <pelle.koster@nginfra.nl>
2021-03-24 10:58:55 +0100

    Make sure model engine installs extra requirements

--- bin/commit-version-to-modelengine-repo.sh
+++ bin/commit-version-to-modelengine-repo.sh
@@ -17,11 +17,12 @@ cd model_engine
 git config user.email "${PIPELINE_RUNNER_EMAIL}"
 git config user.name "${PIPELINE_RUNNER_NAME}"
 
+SELF_AS_REQUIREMENT="${CI_PROJECT_NAME}[models]>=${REPO_VERSION}"
 # if already in version file, replace
 if grep -q $CI_PROJECT_NAME $REQUIREMENTS_FILE_NAME; then
-  sed -i "s/.*$CI_PROJECT_NAME.*/$CI_PROJECT_NAME>=$REPO_VERSION/g" $REQUIREMENTS_FILE_NAME
+  sed -i "s/.*$CI_PROJECT_NAME.*/${SELF_AS_REQUIREMENT}/g" $REQUIREMENTS_FILE_NAME
 else # else add
-  echo "$CI_PROJECT_NAME>=$REPO_VERSION" >> $REQUIREMENTS_FILE_NAME
+  echo "${SELF_AS_REQUIREMENT}" >> $REQUIREMENTS_FILE_NAME
 fi
 
 # Git commit without any changes returns exit status 1
@@ -30,6 +31,6 @@ if ! git diff-index --quiet HEAD --; then
   # Need to update all the versions in the model-engine
   make bump-version
   git add --all
-  git commit -m "Update requirements of of ${CI_PROJECT_NAME} in model_engine"
+  git commit -m "Update requirements of ${CI_PROJECT_NAME}:${REPO_VERSION} in model_engine"
   git push
 fi
