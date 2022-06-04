ffa2f38d12d4372903b5efa49b13e84b9c53a282
Pelle Koster <pelle.koster@nginfra.nl>
2022-06-04 16:29:12 +0200

    bump version

--- movici_simulation_core/__init__.py
+++ movici_simulation_core/__init__.py
@@ -2,4 +2,4 @@ from .simulation import Simulation
 
 __all__ = ["Simulation"]
 
-__version__ = "2.5.1"
+__version__ = "2.5.2"
--- movici_simulation_core/json_schemas/models/legacy/__init__.py
+++
@@ -1,5 +0,0 @@
-import pathlib
-
-SCHEMA_PATH = PATH = pathlib.Path(__file__).parent
-
-__all__ = ["SCHEMA_PATH", "PATH"]
