5f2142039e72286a34c91c344bc67f08fec5bb3e
Pelle Koster <pelle.koster@nginfra.nl>
2022-09-20 12:42:17 +0200

    fixt doctest, add model as plugin

--- movici_simulation_core/preprocessing/__init__.py
+++ movici_simulation_core/preprocessing/__init__.py
@@ -1,4 +1,10 @@
-from .data_sources import DataSource, GeopandasSource, NetCDFGridSource, NumpyDataSource
+from .data_sources import (
+    DataSource,
+    GeopandasSource,
+    NetCDFGridSource,
+    NumpyDataSource,
+    PandasDataSource,
+)
 from .dataset_creator import DatasetCreator, create_dataset, get_dataset_creator_schema
 from .tapefile import InterpolatingTapefile, TimeDependentAttribute
 
