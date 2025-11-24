6ba5c3b84ae3bd7f24c2745f2a88b011adebd8d5
Pelle Koster <pellekoster@octagonal.nl>
2025-11-24 11:35:32 +0100

    Suppress Aequilibrae log message on import regarding default parameters being used

--- movici_simulation_core/integrations/ae/__init__.py
+++ movici_simulation_core/integrations/ae/__init__.py
@@ -1,3 +1,11 @@
+# ruff: noqa: E402
+import logging
+
+# filter log message emited on aequilibrae import. This needs to be done before importing
+# aequilibrae. See https://github.com/AequilibraE/aequilibrae/issues/734
+logger = logging.getLogger("aequilibrae")
+logger.addFilter(lambda r: not r.msg.startswith("No pre-existing parameter"))
+
 from .collections import AssignmentResultCollection, GraphPath, LinkCollection, NodeCollection
 from .id_generator import IdGenerator
 from .point_generator import PointGenerator
