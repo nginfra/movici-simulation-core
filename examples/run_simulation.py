b10ab545f13d0d6b17165411ed5c7e09fde043fe
Pelle Koster <pellekoster@octagonal.nl>
2025-11-27 15:57:51 +0100

    fix example using orjson dumps as bytes

--- examples/run_simulation.py
+++ examples/run_simulation.py
@@ -44,7 +44,7 @@ class DummyModel(TrackedModel):
         if self.attr.flags & PUB:
             self.attr[0] = 1.0
         else:
-            (Path(tempfile.tempdir) / "test.json").write_text(
+            (Path(tempfile.tempdir) / "test.json").write_bytes(
                 EntityInitDataFormat().dumps(state.to_dict())
             )
 
