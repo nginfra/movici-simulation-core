f0a725df26e6edd25568056a06b0adc7b1115dad
Pelle Koster <pellekoster@octagonal.nl>
2026-07-07 08:34:03 +0200

    cleanup api __init__.py

--- packages/data-core/src/movici_data_core/api/__init__.py
+++ packages/data-core/src/movici_data_core/api/__init__.py
@@ -4,6 +4,7 @@ import asyncio
 import contextlib
 import logging
 import pathlib
+import tempfile
 import typing as t
 
 import fastapi
@@ -11,7 +12,6 @@ from fastapi import APIRouter, FastAPI, Request
 
 from movici_data_core.api.dependencies import SQLALCHEMY_SERVER_KEY
 from movici_data_core.database.backend import SQLAlchemyServer
-from movici_data_core.database.model import DatabaseMode
 from movici_data_core.exceptions import MoviciDataError
 
 from .datasets import dataset_router
@@ -25,12 +25,6 @@ from .schema import (
 from .updates import update_router
 from .workspaces import workspace_router
 
-# TODO: make this part of MoviciWebApiBuilder (or similar builder/factory class)
-SQLITE_DB_FILE = "movici.db"
-DBAPI_URL = f"sqlite+aiosqlite:///{SQLITE_DB_FILE}"
-TMPFILE_DIR = "tmp"
-
-DATABASE_MODE = DatabaseMode.SINGLE_SCENARIO
 DEFAULT_ROUTERS = (
     attribute_type_router,
     dataset_router,
@@ -41,8 +35,8 @@ DEFAULT_ROUTERS = (
     update_router,
     scenario_router,
 )
+
 logger = logging.getLogger(__name__)
-logging.basicConfig()
 
 
 def make_app(server: SQLAlchemyServer, routers: t.Iterable[APIRouter] | None = None):
@@ -74,11 +68,11 @@ def make_app(server: SQLAlchemyServer, routers: t.Iterable[APIRouter] | None = N
 
 
 def make_default_app():
-    tmpfile_path = pathlib.Path(TMPFILE_DIR)
-    tmpfile_path.mkdir(parents=True, exist_ok=True)
+    tmpfile_path = pathlib.Path(tempfile.mkdtemp(prefix="movici_api_tmp_"))
 
-    db_path = pathlib.Path(SQLITE_DB_FILE)
-    server = SQLAlchemyServer(DBAPI_URL, tmpfile_dir=tmpfile_path)
+    db_path = pathlib.Path("movici.db`")
+    dbapi_url = f"sqlite+aiosqlite:///{db_path}"
+    server = SQLAlchemyServer(dbapi_url, tmpfile_dir=tmpfile_path)
     if not db_path.exists():
 
         async def setup():
@@ -89,4 +83,6 @@ def make_default_app():
     return make_app(server)
 
 
-app = make_default_app()
+if __name__ == "__main__":
+    logging.basicConfig()
+    app = make_default_app()
