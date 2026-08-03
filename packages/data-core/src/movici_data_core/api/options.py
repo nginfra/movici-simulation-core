ad45a2e07be7a47b6a75a11889b3e5bf992e047d
Pelle Koster <pellekoster@octagonal.nl>
2026-08-03 11:14:45 +0200

    implement endpoint for reading and updating database options

---
+++ packages/data-core/src/movici_data_core/api/options.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter
+
+from movici_data_core.marshalling import OperationSuccess, OptionsIn, OptionsOut
+
+from .dependencies import DepBackend
+
+options_router = APIRouter(prefix="/options")
+
+
+@options_router.get("")
+async def get_scenarios(backend: DepBackend) -> OptionsOut:
+    options = backend.options
+    return OptionsOut(
+        mode=options.mode,
+        strict_dataset_types=options.STRICT_DATASET_TYPES,
+        strict_entity_types=options.STRICT_ENTITY_TYPES,
+        strict_attribute_types=options.STRICT_ATTRIBUTE_TYPES,
+        strict_model_types=options.STRICT_MODEL_TYPES,
+        strict_scenario_datasets=options.STRICT_SCENARIO_DATASETS,
+        immutable_workspace_names=options.IMMUTABLE_WORKSPACE_NAMES,
+    )
+
+
+@options_router.put("")
+async def update_scenario(
+    options: OptionsIn,
+    backend: DepBackend,
+) -> OperationSuccess:
+    if options.mode is not None:
+        await backend.set_database_mode(options.mode)
+    backend.set_options(
+        strict_dataset_types=options.strict_dataset_types,
+        strict_entity_types=options.strict_entity_types,
+        strict_attribute_types=options.strict_attribute_types,
+        strict_model_types=options.strict_model_types,
+        strict_scenario_datasets=options.strict_scenario_datasets,
+        immutable_workspace_names=options.immutable_workspace_names,
+    )
+    return OperationSuccess.for_path_operation(resource="options", verb="updated")
---
+++ packages/data-core/tests/api/test_options.py
@@ -0,0 +1,42 @@
+import pytest
+
+
+def test_get_options(get_json):
+    assert get_json("/options") == {
+        "mode": "multiple_workspaces",
+        "strict_dataset_types": True,
+        "strict_attribute_types": True,
+        "strict_entity_types": True,
+        "strict_model_types": True,
+        "strict_scenario_datasets": True,
+        "immutable_workspace_names": False,
+    }
+
+
+def test_get_and_update_database_mode(get_json):
+    current_options = get_json("/options")
+    assert current_options["mode"] != "single_scenario"
+    get_json("/options", method="PUT", json={"mode": "single_scenario"})
+
+    result = get_json("/options")
+    assert result == {**current_options, "mode": "single_scenario"}
+
+
+@pytest.mark.parametrize(
+    "option, value",
+    [
+        ("strict_dataset_types", False),
+        ("strict_attribute_types", False),
+        ("strict_entity_types", False),
+        ("strict_model_types", False),
+        ("strict_scenario_datasets", False),
+        ("immutable_workspace_names", True),
+    ],
+)
+def test_get_and_update_options(get_json, option, value):
+    current_options = get_json("/options")
+    assert current_options[option] != value
+    get_json("/options", method="PUT", json={option: value})
+
+    result = get_json("/options")
+    assert result == {**current_options, option: value}
